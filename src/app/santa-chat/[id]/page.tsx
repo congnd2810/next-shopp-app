cf3b9795cee6a0eda0a41c549f624f06f561d71e
cong nguyen <nguyenduccong28102000@gmail.com>
2023-11-06 23:56:45 +0700

    update chat

--- src/app/santa-chat/[id]/page.tsx
+++ src/app/santa-chat/[id]/page.tsx
@@ -1,56 +1,42 @@
 'use client'
 
-import { Calligraffitti } from "next/font/google";
 import { useEffect, useState } from "react"
+import data from '../../data/data.json'
 
-async function getData() {
-  try {
-    const res = await fetch('https://api-chat-santa.fun-apps.net/listSanta');
 
-    if (!res.ok) {
-      throw new Error('Failed to fetch data');
-    }
-
-    return res.json();
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-const headers = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-  "Access-Control-Allow-Headers": "Content-Type",
-};
-
-export default function Chat() {
+export default function Chat({ params }: { params: { id: string } }) {
   const [conversation, setConversation] = useState<any>([])
   const [newMessageStatus, setNewMessageStatus] = useState(true)
 
-  const host = "https://api-chat-santa.fun-apps.net/conversationv2"
-  let params: any = {
-    id: "8eb93d93-5bf5-490d-8a9d-102b2a415a16"
-  }
+  let santaObj = data.find((obj) => obj.id === params.id);
+  console.log(santaObj)
 
-  const init = {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json;charset=UTF-8',
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-      "Access-Control-Allow-Headers": "Content-Type"
-    },
-    body: JSON.stringify(params),
-  };
+  const host = "https://api-chat-santa.fun-apps.net/conversationv2"
   
   // await fetch(host, init).then((response) => response.text()).then((responseText) => console.log(responseText));
 
 
   useEffect(() => {
+    let paramsRequest: any = {
+      id: "8eb93d93-5bf5-490d-8a9d-102b2a415a16"
+    }
+    if (conversation.length > 0) paramsRequest["prompt"] = conversation
+    console.log(paramsRequest)
+    let init = {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json;charset=UTF-8',
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+        "Access-Control-Allow-Headers": "Content-Type"
+      },
+      body: JSON.stringify(paramsRequest),
+    };
+
     const fetchData = async () => {
       const posts = await fetch(host, init);
       const resText = await posts.text()
-      console.log(resText)
+      // console.log(resText)
       setConversation([{ role: "assistant", content: resText }])
     };
 
@@ -58,7 +44,7 @@ export default function Chat() {
   }, [newMessageStatus])
 
   useEffect(() => {
-    console.log(conversation);
+    // console.log(conversation);
   }, [conversation]);
 
   return (
@@ -71,13 +57,13 @@ export default function Chat() {
                     <circle cx="8" cy="8" r="8" fill="currentColor"></circle>
                 </svg>
               </span>
-          <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="" className="w-10 sm:w-16 h-10 sm:h-16 rounded-full" />
+          <img src={santaObj?.image} alt="" className="w-10 sm:w-16 h-10 sm:h-16 rounded-full" />
           </div>
           <div className="flex flex-col leading-tight">
               <div className="text-2xl mt-1 flex items-center">
-                <span className="text-gray-700 mr-3">Anderson Vanhron</span>
+                <span className="text-gray-700 mr-3">{santaObj?.name}</span>
               </div>
-              <span className="text-lg text-gray-600">Junior Developer</span>
+              <span className="text-lg text-gray-600">from {santaObj?.nationality}</span>
           </div>
         </div>
         <div className="flex items-center space-x-2">
@@ -114,75 +100,7 @@ export default function Chat() {
               </div>
               <img src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-2" />
           </div>
-        </div>
-        <div className="chat-message">
-          <div className="flex items-end">
-              <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
-                <div><span className="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">Command was run with root privileges. I'm sure about that.</span></div>
-                <div><span className="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">I've update the description so it's more obviously now</span></div>
-                <div><span className="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">FYI https://askubuntu.com/a/700266/510172</span></div>
-                <div>
-                    <span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">
-                      Check the line above (it ends with a # so, I'm running it as root )
-                      <pre># npm install -g @vue/devtools</pre>
-                    </span>
-                </div>
-              </div>
-              <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-1" />
-          </div>
-        </div>
-        <div className="chat-message">
-          <div className="flex items-end justify-end">
-              <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
-                <div><span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">Any updates on this issue? I'm getting the same error when trying to install devtools. Thanks</span></div>
-              </div>
-              <img src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-2" />
-          </div>
-        </div>
-        <div className="chat-message">
-          <div className="flex items-end">
-              <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
-                <div><span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">Thanks for your message David. I thought I'm alone with this issue. Please, ? the issue to support it :)</span></div>
-              </div>
-              <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-1" />
-          </div>
-        </div>
-        <div className="chat-message">
-          <div className="flex items-end justify-end">
-              <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
-                <div><span className="px-4 py-2 rounded-lg inline-block bg-blue-600 text-white ">Are you using sudo?</span></div>
-                {/* <div><span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">Run this command sudo chown -R `whoami` /Users/{{your_user_profile}}/.npm-global/ then install the package globally without using sudo</span></div> */}
-              </div>
-              <img src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-2" />
-          </div>
-        </div>
-        <div className="chat-message">
-          <div className="flex items-end">
-              <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
-                <div><span className="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">It seems like you are from Mac OS world. There is no /Users/ folder on linux ?</span></div>
-                <div><span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">I have no issue with any other packages installed with root permission globally.</span></div>
-              </div>
-              <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-1" />
-          </div>
-        </div>
-        <div className="chat-message">
-          <div className="flex items-end justify-end">
-              <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
-                <div><span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">yes, I have a mac. I never had issues with root permission as well, but this helped me to solve the problem</span></div>
-              </div>
-              <img src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-2" />
-          </div>
-        </div>
-        <div className="chat-message">
-          <div className="flex items-end">
-              <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
-                <div><span className="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">I get the same error on Arch Linux (also with sudo)</span></div>
-                <div><span className="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">I also have this issue, Here is what I was doing until now: #1076</span></div>
-                <div><span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">even i am facing</span></div>
-              </div>
-              <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-1" />
-          </div>
-        </div>
+        </div>       
     </div>
     <div className="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
         <div className="relative flex">
