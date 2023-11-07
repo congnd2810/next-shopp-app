9c33d3c45bc8a09c6986aea2e077879f63ed9363
unknown <congnd@solarapp.asia>
2023-11-07 15:38:48 +0700

    update

--- src/app/santa-chat/[id]/page.tsx
+++ src/app/santa-chat/[id]/page.tsx
@@ -6,46 +6,55 @@ import data from '../../data/data.json'
 
 export default function Chat({ params }: { params: { id: string } }) {
   const [conversation, setConversation] = useState<any>([])
-  const [newMessageStatus, setNewMessageStatus] = useState(true)
+  const [statusSendClick, setStatusSendClick] = useState(false)
+  const [userNewMessage, setUserNewMessage] = useState('')
+  const [assistantMessages, setAssistantMessages] = useState([])
+
+  const handleSendClick = () => {
+    let temp = conversation
+    temp.push({ role: "user", content: userNewMessage })
+    setConversation(temp)
+    setStatusSendClick(!statusSendClick);
+    setUserNewMessage('')
+  };
+
+  const handleInputChange = (e: any) => {
+    setUserNewMessage(e.target.value); // Cập nhật giá trị state theo giá trị trường input
+  };
 
   let santaObj = data.find((obj) => obj.id === params.id);
-  console.log(santaObj)
 
   const host = "https://api-chat-santa.fun-apps.net/conversationv2"
   
+  let paramsRequest: any = {
+    id: params.id
+  }
   // await fetch(host, init).then((response) => response.text()).then((responseText) => console.log(responseText));
 
 
   useEffect(() => {
-    let paramsRequest: any = {
-      id: "8eb93d93-5bf5-490d-8a9d-102b2a415a16"
-    }
+
     if (conversation.length > 0) paramsRequest["prompt"] = conversation
+
     console.log(paramsRequest)
     let init = {
       method: 'POST',
       headers: {
-        'content-type': 'application/json;charset=UTF-8',
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-        "Access-Control-Allow-Headers": "Content-Type"
+        'content-type': 'application/json;charset=UTF-8'
       },
       body: JSON.stringify(paramsRequest),
     };
 
     const fetchData = async () => {
       const posts = await fetch(host, init);
-      const resText = await posts.text()
-      // console.log(resText)
-      setConversation([{ role: "assistant", content: resText }])
+      const resText = await posts.json()
+      let newConversation = [...conversation];
+      newConversation.push({ role: "assistant", content: resText })
+      setConversation(newConversation)
     };
 
     fetchData()
-  }, [newMessageStatus])
-
-  useEffect(() => {
-    // console.log(conversation);
-  }, [conversation]);
+  }, [statusSendClick])
 
   return (
     <div className="flex-1 p:2 sm:p-6 justify-between flex flex-col h-screen">
@@ -85,22 +94,34 @@ export default function Chat({ params }: { params: { id: string } }) {
         </div>
     </div>
     <div id="messages" className="flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
-        <div className="chat-message">
-          <div className="flex items-end">
-              <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
-                <div><span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">Can be verified on any platform using docker</span></div>
-              </div>
-              <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-1" />
-          </div>
-        </div>
-        <div className="chat-message">
-          <div className="flex items-end justify-end">
-              <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
-                <div><span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">Your error message says permission denied, npm global installs must be given root privileges.</span></div>
-              </div>
-              <img src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-2" />
-          </div>
-        </div>       
+        {
+          conversation.map((objMessage: any) => {
+            if (objMessage.role === "assistant") {
+              return (
+                <div className="chat-message">
+                  <div className="flex items-end">
+                      <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
+                        <div><span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">{objMessage.content}</span></div>
+                      </div>
+                      <img src={santaObj?.image} alt="My profile" className="w-6 h-6 rounded-full order-1" />
+                  </div>
+                </div>
+              )
+            }
+            else {
+              return (
+                <div className="chat-message">
+                  <div className="flex items-end justify-end">
+                      <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
+                        <div><span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">{objMessage.content}</span></div>
+                      </div>
+                      <img src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-2" />
+                  </div>
+                </div>
+              )
+            }
+          })
+        }
     </div>
     <div className="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
         <div className="relative flex">
@@ -111,7 +132,7 @@ export default function Chat({ params }: { params: { id: string } }) {
                 </svg>
               </button>
           </span>
-          <input type="text" placeholder="Write your message!" className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-md py-3" />
+          <input type="text" placeholder="Write your message!" value={userNewMessage} onChange={handleInputChange} className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-md py-3" />
           <div className="absolute right-0 items-center inset-y-0 hidden sm:flex">
               <button type="button" className="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-gray-600">
@@ -129,7 +150,7 @@ export default function Chat({ params }: { params: { id: string } }) {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                 </svg>
               </button>
-              <button type="button" id="send" className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none">
+              <button type="button" id="send" onClick={handleSendClick} className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none">
                 <span className="font-bold">Send</span>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6 ml-2 transform rotate-90">
                     <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
